79fc89bdfabacd9383dc47646f93f6ec8e713dd4
smithasreelathamohan <smitha.s11may@gmail.com>
2026-05-12 01:30:54 +0200

    allure report to open from github

--- src/test/java/com/example/projectrecap/CalculatorTest.java
+++ src/test/java/com/example/projectrecap/CalculatorTest.java
@@ -55,7 +55,7 @@ class CalculatorTest {
     @CsvSource({
             "10,2,5",
             "20,4,5",
-            "9,3,3"
+            "9,3,8"
     })
     void divideTest(int a, int b, int expected) {
         int result = calc.divide(a, b);
